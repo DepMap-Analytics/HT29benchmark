556667b25bd4c93d2d838fb622be5eca14482581
Raffaele Iannuzzi <raffaele.iannuzzi@fht.org>
2022-05-16 16:54:44 +0200

    "download_ref_dataset" function added

--- R/HT29benchmark.R
+++ R/HT29benchmark.R
@@ -1,4 +1,30 @@
 
+HT29R.download_ref_dataset <- function(whatToDownload='FCs',
+                                     destFolder='./',
+                                     dataRepoURL = "https://cog.sanger.ac.uk/cmp/downloads/crispr_cas9_benchmark/",
+                                     expNames=c("HT29_c903","HT29_c904","HT29_c905","HT29_c906","HT29_c907","HT29_c908")){
+  nexp <- length(expNames)
+
+  for (i in 1:nexp){
+    if (whatToDownload != 'rawCounts' &
+        whatToDownload != 'FCs'){
+      stop('Available data types (to be specified in whatToDownload): normalised sgRNA depletion fold-changes ("FCs") and rawCounts ("rawCounts")')
+    }
+
+    print(paste('Downloading data for ',expNames[i],' experiment (',i,' of ',nexp,')',sep=''))
+
+    if (whatToDownload == 'FCs'){
+      download.file(url = paste(dataRepoURL,'01_normalised_and_FCs/',expNames[i],'.tsv_foldChanges.Rdata',sep=''),
+                    destfile = paste(destFolder,'/',expNames[i],'_foldChanges.Rdata',sep=''))
+    }
+
+    if (whatToDownload == 'rawCounts'){
+      download.file(url = paste(dataRepoURL,'00_rawCounts%20assembled/',expNames[i],'.tsv',sep=''),
+                    destfile = paste(destFolder,'/',expNames[i],'.tsv',sep=''))
+    }
+  }
+}
+
 HT29R.FC_distributions <- function(refDataDir='./',resDir='./',userFCs=NULL){
 
 
